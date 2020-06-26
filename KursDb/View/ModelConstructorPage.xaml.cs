e4d3d6fbba502c1e19dd2f70d5a97522729fbc94
garinich777 <62898482+garinich777@users.noreply.github.com>
2020-06-26 14:33:37 +0300

    Добавлен дизайн окна работы с фурнитурой

--- KursDb/View/ModelConstructorPage.xaml.cs
+++ KursDb/View/ModelConstructorPage.xaml.cs
@@ -13,5 +13,10 @@ namespace KursDb.View
             InitializeComponent();
             DataContext = VM;
         }
+
+        private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
     }
 }
